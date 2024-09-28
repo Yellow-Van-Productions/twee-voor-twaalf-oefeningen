1ec9f1bd2f010b603ebe22ee8a36e68e73678ac4
Jelle <37933621+OswaldTheMighty@users.noreply.github.com>
2024-09-28 23:22:41 +0200

    Paardensprong werkt

--- src/Components/Paardensprong.tsx
+++ src/Components/Paardensprong.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Suspense, useEffect, useMemo, useState } from "react";
 
 import "./styles.css";
 import Grid from "@mui/material/Grid2";
-import { TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
+import { Button, CircularProgress, TextField, Typography } from "@mui/material";
 
 type Char =
   | "a"
@@ -30,47 +30,145 @@ type Char =
   | "w"
   | "x"
   | "y"
-  | "z";
+  | "z"
+  | "?";
+
+function toChar(letter: string): Char {
+  if (letter.length === 1) {
+    return letter as Char;
+  }
+  return "?";
+}
 
 interface IPaardensprongTableProps {
   achtLetterWoord: Array<Char>;
 }
 
 function PaardensprongTable(props: IPaardensprongTableProps) {
-  return (
+  return props.achtLetterWoord ? (
     <table>
       <tbody>
         <tr>
-          <td className="square">{props.achtLetterWoord[0]}</td>
-          <td className="square">{props.achtLetterWoord[1]}</td>
-          <td className="square">{props.achtLetterWoord[2]}</td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[0]}</Typography>
+          </td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[1]}</Typography>
+          </td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[2]}</Typography>
+          </td>
         </tr>
         <tr>
-          <td className="square">{props.achtLetterWoord[3]}</td>
-          <td className="square center-piece">...</td>
-          <td className="square">{props.achtLetterWoord[4]}</td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[3]}</Typography>
+          </td>
+          <td className="square center-piece"></td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[4]}</Typography>
+          </td>
         </tr>
         <tr>
-          <td className="square">{props.achtLetterWoord[5]}</td>
-          <td className="square">{props.achtLetterWoord[6]}</td>
-          <td className="square">{props.achtLetterWoord[7]}</td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[5]}</Typography>
+          </td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[6]}</Typography>
+          </td>
+          <td className="square">
+            <Typography variant="h3">{props.achtLetterWoord[7]}</Typography>
+          </td>
         </tr>
       </tbody>
     </table>
+  ) : (
+    <></>
   );
 }
 
+enum Direction {
+  ClockWise,
+  CounterClockwise,
+}
+
+interface IPaardenSprongPuzzle {
+  direction: Direction;
+  startPosition: number;
+  word: string;
+  scrambledWord: Array<Char>;
+}
+
+function scrambleWord(
+  direction: Direction,
+  word: string,
+  startPosition: number
+): IPaardenSprongPuzzle {
+  const CLOCK_WISE_PAARDENSPRONG = [0, 4, 5, 1, 7, 3, 2, 6];
+
+  const scrambledWordArray = new Array<Char>(8);
+  let index = CLOCK_WISE_PAARDENSPRONG.indexOf(startPosition);
+  for (const letter of word) {
+    scrambledWordArray[CLOCK_WISE_PAARDENSPRONG[index]] = toChar(letter);
+    if (direction === Direction.ClockWise) {
+      index++;
+      if (index > 7) {
+        index -= 8;
+      }
+    } else {
+      index--;
+      if (index < 0) {
+        index += 8;
+      }
+    }
+  }
+
+  return {
+    direction: direction,
+    word: word,
+    startPosition: startPosition,
+    scrambledWord: scrambledWordArray,
+  };
+}
+
 export default function Paardensprong() {
-  const achtLetterWoordScrambled = new Array<Char>(
-    "a",
-    "a",
-    "a",
-    "a",
-    "a",
-    "a",
-    "a",
-    "a"
-  );
+  const [eightLetterWordDatabase, setEightLetterWordDatabase] = useState<
+    string[] | null
+  >(null);
+  const [puzzle, setPuzzle] = useState<IPaardenSprongPuzzle | null>(null);
+
+  useEffect(() => {
+    const loadDatabaseAsync = async () => {
+      const fileContents = await fetch("achtletterwoorden.csv");
+      const words = await fileContents.text();
+      const listOfWords = words.split("\r");
+      setEightLetterWordDatabase(listOfWords);
+    };
+
+    loadDatabaseAsync();
+  }, []);
+
+  useEffect(() => {
+    generateNewWord(eightLetterWordDatabase);
+  }, [eightLetterWordDatabase]);
+
+  const generateNewWord = (listOfWords: string[] | null) => {
+    if (!listOfWords || listOfWords.length === 0) {
+      return;
+    }
+
+    const randomWord =
+      listOfWords[Math.floor(Math.random() * listOfWords.length)];
+    const randomDirection =
+      Math.random() < 0.5 ? Direction.ClockWise : Direction.CounterClockwise;
+    const randomStartPosition = Math.floor(Math.random() * 7);
+
+    const newPuzzle = scrambleWord(
+      randomDirection,
+      randomWord,
+      randomStartPosition
+    );
+    setPuzzle(newPuzzle);
+  };
 
   return (
     <Grid
@@ -79,13 +177,31 @@ export default function Paardensprong() {
       direction="column"
       alignItems="center"
       justifyContent="center"
+      margin={3}
     >
       <Grid>
-        <PaardensprongTable achtLetterWoord={achtLetterWoordScrambled} />
+        <Button
+          variant="contained"
+          onClick={() => {
+            generateNewWord(eightLetterWordDatabase);
+          }}
+        >
+          Nieuwe Paardensprong
+        </Button>
+      </Grid>
+      <Grid>
+        {puzzle ? (
+          <PaardensprongTable achtLetterWoord={puzzle.scrambledWord} />
+        ) : (
+          <CircularProgress />
+        )}
       </Grid>
 
       <Grid>
-        <input></input>
+        <Grid container direction="row" spacing={1}>
+          <TextField id="outlined-basic" label="Antwoord" variant="outlined" />
+          <Button variant="contained">Check</Button>
+        </Grid>
       </Grid>
     </Grid>
   );
