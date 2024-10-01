126f333f29ec651d473f596bd531ada4c08e583d
Jelle <37933621+OswaldTheMighty@users.noreply.github.com>
2024-10-01 13:20:44 +0200

    Add "Under construction" for taartpunt

--- src/Components/Taartpunt.tsx
+++ src/Components/Taartpunt.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import { Container, Typography } from "@mui/material";
+
 export default function Taartpunt() {
-  return <></>;
+  return (
+    <Container>
+      <Typography variant="h5">Under construction...</Typography>
+    </Container>
+  );
 }
